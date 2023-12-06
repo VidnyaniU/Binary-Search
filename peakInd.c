d5180fb8cee7ec4bdb8d08530238faadc37442bf
VidnyaniU <vidnyani.umathe@gmail.com>
2023-12-06 09:47:23 +0530

    updated peakInd.c
    Added Binary approach to find the peak element index in a bitonic array.

--- peakInd.c
+++ peakInd.c
@@ -15,10 +15,40 @@ int findPeakIndLinear(int *arr, int size)
     }
     return -1;
 }
+
+// Binary search approach takes O(log n)
+
+int findPeakIndBinary(int *arr, int size)
+{
+    int start = 0;
+    int end = size - 1;
+    while (start < end)
+    {
+        int mid = start + (end - start) / 2;
+        if (arr[mid] > arr[mid + 1])
+        {
+            // this means that we are in decreasing part of the array and the peak element lies in the left part
+            // it can be mid as well thatswhy no mid -1
+            end = mid;
+        }
+        else
+        {
+            // this means that we are in increasing part of the array and the peak element lies in the right part
+
+            start = mid + 1;
+        }
+    }
+
+    // when start == end we will get our peak index
+    return start; // or return end
+}
+
 int main()
 {
     int arr[] = {1, 2, 5, 4, 2};
     int size = sizeof(arr) / sizeof(arr[0]);
-    printf("%d ", findPeakIndLinear(arr, size));
+    // printf("%d ", findPeakIndLinear(arr, size));
+    printf("%d ", findPeakIndBinary(arr, size));
+
     return 0;
 }
