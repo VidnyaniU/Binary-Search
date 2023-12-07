9eef63442f30f682e80450219c44c59edabd191f
VidnyaniU <150503254+VidnyaniU@users.noreply.github.com>
2023-12-08 00:28:07 +0530

    Update rotatedArray.c
    added findPivotWithDuplicate function.

--- rotatedArray.c
+++ rotatedArray.c
@@ -52,7 +52,55 @@ int findPivot(int *arr, int length)
     }
     return -1;
 }
+// for with duplicates
+int findPivotWithDuplicates(int *arr, int length)
+{
+    int start = 0;
+    int end = length - 1;
+    while (start <= end)
+    {
+        int mid = start + (end - start) / 2;
+        if (mid < end && arr[mid] > arr[mid + 1])
+        {
+            return mid;
+        }
 
+        if (mid > start && arr[mid] < arr[mid - 1])
+        {
+            return mid - 1;
+        }
+
+        if (arr[mid] <= arr[0])
+        {
+            end = mid - 1;
+        }
+        else
+        {
+            start = mid + 1;
+        }
+
+        if(arr[mid]==arr[start] && arr[mid]==arr[end])
+        {
+            if(start <end && arr[start] >arr[start+1]){
+                return start;
+            }
+            start++;
+            if(end >start && arr[end] < arr[end-1]){
+                return end -1;
+                
+            }
+            end --;
+        }
+        else if(arr[start] <arr [mid] || (arr[start]==arr[mid] && arr[mid] >arr[end])){
+            start = mid +1;
+            
+        }
+        else{
+        end = mid -1;
+        }
+    }
+    return -1;
+}
 int search(int *arr, int length, int target)
 {
 
