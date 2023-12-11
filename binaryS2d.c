ca221ec4458c064374a6e1e25ae980118fb1a99b
VidnyaniU <vidnyani.umathe@gmail.com>
2023-12-11 09:26:03 +0530

    updated  binaryS2d.c
    Returned the result using structure.

--- binaryS2d.c
+++ binaryS2d.c
@@ -1,17 +1,30 @@
 #include <stdio.h>
 
-void binaryS2D(int arr[4][4], int target)
+struct rowCol
+{
+    int row, col;
+};
+
+typedef struct rowCol Struct;
+
+Struct binaryS2D(int arr[4][4], int target)
 {
     int row = 0;
     int col = 3;
+
+    Struct s;
+
     while (row < sizeof(arr[0]) && col >= 0)
     {
+        s.row = row;
+        s.col = col;
+
         if (arr[row][col] == target)
-        // return row, col;
-        {
-            printf("Element found at %d %d.\n", row, col);
-            break;
-        }
+            return s;
+        // {
+        //     printf("Element found at %d %d.\n", row, col);
+        //     break;
+        // }
 
         else if (arr[row][col] > target)
             col--;
@@ -19,12 +32,15 @@ void binaryS2D(int arr[4][4], int target)
         else
             row++;
     }
-    // return {row, col};
+    return s;
 }
 int main()
 {
     int arr[4][4] = {{10, 20, 30, 40}, {15, 25, 35, 45}, {28, 29, 37, 49}, {33, 34, 38, 50}};
-    binaryS2D(arr, 37);
+
+    Struct res = binaryS2D(arr, 37);
+
+    printf("%d %d", res.row, res.col);
 
     return 0;
 }
